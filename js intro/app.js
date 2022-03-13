d0fabc18d4db00722c3aa1ece4e4ab4b224b8c26
Ashish Shukla <akshkgd@gmail.com>
2022-03-13 16:30:09 +0530

    js intro

---
+++ js intro/app.js
@@ -0,0 +1,16 @@
+let name = 'ashish';
+var email = 'ashish@codekaro.in';
+console.log(name);
+console.log(email);
+
+if(true){
+    var userId = 12345;
+    let password = 'ashish@123';
+    console.log(name);
+    console.log(email);
+    console.log(password);
+
+}
+let password = 'ashish@123New';
+console.log(userId);
+console.log(password);
---
+++ js intro/dataTypes.js
@@ -0,0 +1,33 @@
+// data types -> string, numbers, boolean, null, undefined, object, array;
+
+
+// let name = "ashish";
+// name = null;
+// console.log(name);
+// console.log(typeof name);
+
+
+// let age = 24.45;
+// console.log(age);
+// console.log(typeof age);
+
+
+// let isActive = true;
+// console.log(isActive);
+// console.log(typeof isActive);
+
+
+// let email;
+// console.log(email);
+// console.log(typeof email);
+
+
+let hobbies = ['cricket', 'football', 'coding'];
+console.log(hobbies);
+// console.log(hobbies[5]);
+// hobbies.push('codekaro');
+// hobbies.pop();
+// hobbies.pop();
+// hobbies[0] = 'teaching';
+hobbies.splice(1,1, 'test', 'test2');
+document.write(hobbies[1]);
