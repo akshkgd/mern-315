bc40667751413c992535cf5e753ff3a9fb5f0a16
Ashish Shukla <akshkgd@gmail.com>
2022-07-12 17:13:02 +0530

    views

--- node js intro/app.js
+++ node js intro/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
 const app = express()
-
-
-app.use('/login', (req,res,next)=>{
-    res.send('<h1>this is login page</h1>')
+app.use(bodyParser.urlencoded({extended:true}))
+app.set('view engine', 'ejs')
+app.get('/login', (req,res,next)=>{
+    res.send('<h1>this is login page</h1>');
 })
-app.use('/', (req,res)=>{
-    res.send('<h1>this is homepage</h1>')
+app.get('/', (req,res)=>{
+    console.log(__dirname);
+    res.sendFile(__dirname + '/index.html')
+})
+app.post('/enrollment',(req,res)=>{
+    console.log(req.body)
+    res.render('enrollmentSuccess', {user: req.body.name, email:req.body.email})
+})
+app.get('*', (req,res)=>{
+    res.send('<h1>Page not found</h1>');
 })
 
 
