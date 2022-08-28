7dfb57309fdf155ae01d9c6cd6c82b7f5f011d4c
Ashish Shukla <akshkgd@gmail.com>
2022-08-28 15:06:56 +0530

    wd

---
+++ login/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs')
+const app = express();
+app.set(('view engine'), 'ejs')
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res)=>{
+    res.render('login', {status: true})
+})
+app.post('/login', (req,res)=>{
+    let adminEmail = 'ashish@gmail.com';
+    let adminPassword = 'admin'
+    if(adminEmail === req.body.email && adminPassword === req.body.password){
+        console.log('login successful');
+        res.render('home')
+    }
+    else{
+        console.log('username or password is wrong')
+        res.render('login', {status: false})
+    }
+})
+
+app.listen(3000, ()=>{
+    console.log('server is running on port 3000')
+})
